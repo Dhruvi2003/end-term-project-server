4bc97fdd73d8725655b0ec94eb852c2e83c99e05
Dhruvi2003 <dhruvi1365.be20@chitkara.edu.in>
2023-02-13 22:41:45 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const { default: mongoose } = require("mongoose");
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+mongoose.connect("mongodb://localhost:27017/gas-booking")
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//port
+const port = process.env.PORT || 8080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
